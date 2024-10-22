88522457d8fa6dfdfa1dd1da73f9467423894b7c
MayurWani12 <mayurw4347@gmail.com>
2024-10-22 19:15:37 +0530

    commit

---
+++ MVC/src/main/java/com/angular/demo/service/EmployeeService.java
@@ -0,0 +1,44 @@
+package com.angular.demo.service;
+
+
+
+import com.angular.demo.modal.Employee;
+import com.angular.demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Optional<Employee> getEmployeeById(int id) {
+        return employeeRepository.findById(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public Employee updateEmployee(int id, Employee employeeDetails) {
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found with id: " + id));
+
+        employee.setName(employeeDetails.getName());
+        employee.setCity(employeeDetails.getCity());
+
+        return employeeRepository.save(employee);
+    }
+
+    public void deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+    }
+}
